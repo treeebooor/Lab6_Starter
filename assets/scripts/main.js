0f8187806cda9d1f165240e7c5adc57c621486c7
treeebooor <robbertjiang@gmail.com>
2021-10-28 22:25:38 -0700

    revert local recipe path

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,9 +6,9 @@ const recipes = [
 	'https://introweb.tech/assets/json/ghostCookies.json',
 	'https://introweb.tech/assets/json/birthdayCake.json',
 	'https://introweb.tech/assets/json/chocolateChip.json',
-	'assets/recipes/brownies.json',
-	'assets/recipes/squash.json',
-	'assets/recipes/pretzel.json',
+	'./assets/recipes/brownies.json',
+	'./assets/recipes/squash.json',
+	'./assets/recipes/pretzel.json',
 ];
 
 // Once all of the recipes that were specified above have been fetched, their
